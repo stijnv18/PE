5edcf2355b4517d6aae9d703786b0ab7ced13b8d
stijnv18 <verheyden.stijn2@gmail.com>
2023-05-24 12:24:42 +0200

    multithreading

--- main.py
+++ main.py
@@ -1,49 +1,44 @@
 import os
 import re
 import sys
-from PIL import Image # pip install Pillow
-import pytesseract # pip install pytesseract
-import cv2 # pip install opencv-python
+from PIL import Image
+import pytesseract
+import cv2
+from concurrent.futures import ThreadPoolExecutor
 
 pytesseract.pytesseract.tesseract_cmd = os.path.expandvars(r"%ProgramFiles%\Tesseract-OCR\tesseract.exe")
 
+def process_frame(frame):
+    text = pytesseract.image_to_string(frame)
+    print(text)
+
 def main():
-	cam = cv2.VideoCapture(0, cv2.CAP_DSHOW) # Try cv2.CAP_AVFOUNDATION if error
+    cam = cv2.VideoCapture("http://192.168.2.149:4747/video/")
 
-	cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-	cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-	if not cam.isOpened():
-		print("Error opening camera")
-		return
+    if not cam.isOpened():
+        print("Error opening camera")
+        return
 
-	while True:
-		ret, frame = cam.read()
-		if not ret:
-			print("Error grabbing the frame")
-			break
-		
-		
-		print(pytesseract.image_to_string(frame))
-		"""
-		print(pytesseract.image_to_string(Image.open(img)))
-		print("-"*100)
-		print(pytesseract.image_to_string(img))
-		print("-"*100)
-		print(pytesseract.image_to_boxes(Image.open(img)))
-		print("-"*100)
-		print(pytesseract.image_to_data(Image.open(img)))
-		print("*"*100)
-		"""
+    with ThreadPoolExecutor(max_workers=64) as executor:
+        while True:
+            ret, frame = cam.read()
+            if not ret:
+                print("Error grabbing the frame")
+                break
 
-		cv2.imshow("Test", frame)
+            executor.submit(process_frame, frame)
 
-		if cv2.waitKey(1) % 256 == ord("q"):
-			print("Quitting...")
-			break
+            cv2.imshow("Test", frame)
 
-	cam.release()
-	cv2.destroyAllWindows()
+            if cv2.waitKey(1) % 256 == ord("q"):
+                print("Quitting...")
+                break
+
+    cam.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-	main()
+    main()
